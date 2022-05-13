5bbd3c0b21e6e0e36bd0271684eb7c8e67168b0c
SzaboBence <szabo.bence070@gmail.com>
2022-05-13 21:22:35 +0200

    oszthatosag pyton feladat

---
+++ pyton/oszthatóság.py
@@ -0,0 +1,10 @@
+bekerszam=int(input("Adj meg egy szamot es megmondom paros e vagy paratlan "))
+osztoszam=int(input("Add meg mien szamal osszak"))
+
+if bekerszam%4==0:
+     print("ëz egy negyel oszthato szam")
+   
+elif bekerszam%osztoszam==0:
+     print("ez egy paros szam")
+else:
+    print("ez egy paratlan szam")    
