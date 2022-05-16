a7dd948ccc485d8ac7f4234387ab909faf52c958
SzaboBence <szabo.bence070@gmail.com>
2022-05-16 13:48:47 +0200

    listafedes program elkeszitese

---
+++ pyton/listaatfedés.py
@@ -0,0 +1,12 @@
+a=[1,1,2,3,5,13,21,34,55,89]
+b=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+l=[]
+for i in b:
+    if i not in a :
+        l.append(i)
+for i in a:
+    if i not in b :
+        l.append(i)
+print(l)
+
+
