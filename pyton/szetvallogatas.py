0812157b4b5c44dd7513992c3851b7f20dcf6bd2
SzaboBence <szabo.bence070@gmail.com>
2022-05-24 19:23:18 +0200

    módisítás

--- pyton/szetvallogatas.py
+++ pyton/szetvallogatas.py
@@ -4,7 +4,7 @@ import imp
 import random
 
 osszesszam=[]
-for n in range(100):
+for n in range(101):
     r=random.randint(0,100)
     osszesszam.append(r)
 
@@ -29,6 +29,20 @@ print(huszonotnelkisseb)
 print(otvenelkisseb)
 print(hetvenotnelkisseb)
 print(szaznalkisseb)
+
+print(osszesszam)
+
+talalt=False
+i=0
+while i<100 and talalt==False:
+    if osszesszam[i]==69:
+        talalt=True
+        print("megtalaltam a 69 ami a lista ",i+1,"elemen van")
+    else:
+        i=i+1  
+
+if talalt==False:
+    print("nincs benne a 69 szam a listba")
 
 
 
