9f37f22caeeba2d7be4bf50c936e2d3e8fb05259
SzaboBence <szabo.bence070@gmail.com>
2022-06-13 15:26:46 +0200

    gyakorlas

--- pyton/deffügveny.py
+++ pyton/deffügveny.py
@@ -1,18 +1,38 @@
 import random
 
-def randomlista(szamok):
-    for n in range(20):
+def randomlista(len_lista):
+    szamok=[]
+    for n in range(len_lista):
        r=random.randint(1,100)
        szamok.append(r)
-
-
-rszamok=[]
-randomlista(rszamok)   
-print(rszamok)    
+       
+    return szamok
 
 
 
-randszamok=[]
-randomlista(randszamok)
+
+
+rszamok=randomlista(20)
+print("randomszamok",rszamok) 
+a=(len(rszamok))-1
+
+
+
+
+randszamok=randomlista(30)
 print(randszamok)
     
+
+def otvenelkisseb(rszamok):
+    otvenelkissebrszamok=[]
+    for n in range(a):
+        if rszamok[n]<=50:
+            otvenelkissebrszamok.append(rszamok[n])
+    print(otvenelkissebrszamok)
+
+otvenelkisseb(rszamok)
+otvenelkisseb(randszamok)       
+
+
+
+
---
+++ pyton/gyakorlofeladat.py
@@ -0,0 +1,41 @@
+import random 
+szam = random.randint(1, 100)
+print("Gondoltam egy számra 1 és 100 között.")
+tipp=0
+while (szam!=tipp):
+    tipp = int(input("Adj meg egy szamot"))
+    if (tipp>szam):
+        print("Nem, ennél kisebbre gondoltam.")
+    else:
+        if (tipp<szam):
+            print("Nem, ennél nagyobbra gondoltam.")
+        else:
+            print("Igen, eltaláltad!") 
+
+
+
+
+
+#masodikfeladat
+
+
+def szamlalo():
+    szoveg=input("milyen szo ")
+    kulonbozokarakterek=input("milyen betu ")
+    szamlal=0
+    szoveghossz=len(szoveg)
+    for n in range(szoveghossz):
+        if szoveg[n]==kulonbozokarakterek:
+            szamlal+=1
+    print("a szoban ennyiszer vann benne a betu: ",szamlal)
+
+szamlalo()  
+
+
+
+#harmadikfeladat
+
+
+
+
+    
